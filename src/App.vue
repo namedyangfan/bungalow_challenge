84988f4320d1226974ca4dc8fc7ad3fc325c07d1
Fan Yang <cedric.yf@gmail.com>
2019-02-23 20:54:13 -0500

    core: install dependences moment, vuetify, axios

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,19 @@
 <template>
-  <div id="app">
-  </div>
+  <v-app>
+    
+  </v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

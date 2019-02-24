ac63261197fa8e2afeeddb6ca242b384303ca51e
Fan Yang <cedric.yf@gmail.com>
2019-02-24 18:22:28 -0500

    feat: add components for filters

---
+++ src/components/Filters.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-btn color="success" v-on:click="changeSelectedCity">Success</v-btn>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Filters',
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    changeSelectedCity: function(){
+      this.$emit('changeSelectedCity', 'chicago')
+    },
+
+    getAvaliableCities: function(){
+      let url = `${process.env.VUE_APP_API_URL}markets/?format=json`
+      this.isLoading = true
+    }
+  },
+  created() {
+  }
+}
+</script>
+
+<style>
+</style>

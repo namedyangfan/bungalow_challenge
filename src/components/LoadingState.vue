f4f92b90322bb403fb5b42fba143fe0d76e16403
Fan Yang <cedric.yf@gmail.com>
2019-03-06 08:47:06 -0500

    fix: use proper center style for loading state and clear filter

--- src/components/LoadingState.vue
+++ src/components/LoadingState.vue
@@ -1,10 +1,15 @@
 <template>
-  <v-container fill-height>
-    <v-layout row wrap align-center class="center-loading">
-      <v-progress-circular :size="100" color="primary" indeterminate>
-      </v-progress-circular>
-    </v-layout >
-  </v-container>
+    <v-card flat tile height="100%" class="pa-0" >
+      <v-layout column fill-height>
+        <v-container fluid fill-height grey lighten-5>
+          <v-layout>
+            <v-flex d-flex justify-center align-center class="text-xs-center">
+              <v-progress-circular :size="100" color="primary" indeterminate></v-progress-circular>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-layout>
+    </v-card>
 </template>
 
 <script>
